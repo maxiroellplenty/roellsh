22a5d26cfc863329a99ac4afc8249c904b261499
Maximilian Röll <maximilian.roell@plentymarkets.com>
2019-04-10 17:01:25 +0200

    # Finished install script & vm script

--- core/scripts/install.sh
+++ core/scripts/install.sh
@@ -7,11 +7,12 @@ SET='\033[0m'
 # Global vars
 aliasPath="";
 installPath=~/rsh;
+rsh=$installPath/rsh.sh;
 
 function checkBashDirs() 
 {
-    bash="~/.bashrc"
-    bash_profile="~/.bashrc_profile"
+    bash=~/.bashrc
+    bash_profile=~/.bashrc_profile
     if [ -f $bash ]; then
         aliasPath=$bash;
     elif [ -f $bash_profile]; then
@@ -22,22 +23,54 @@ function checkBashDirs()
     fi
 }
 
-function addAlias() 
+function printHeader()
 {
-    echo "test"
+cat << "EOF"
+
+   ___  ____  ______   __     ______ __
+  / _ \/ __ \/ __/ /  / /    / __/ // /
+ / , _/ /_/ / _// /__/ /__  _\ \/ _  / 
+/_/|_|\____/___/____/____/ /___/_//_/                                                                            
+
+EOF
+}
+
+function setAlias() 
+{
+    grep -q -F "$rsh" $aliasPath || echo source $rsh >> $aliasPath
 }
 
 function installRsh() 
 {
     mkdir -p $installPath;
     cd $installPath;
-    #curl -LkSs https://codeload.github.com/maxiroellplenty/update-script/legacy.zip/master -o master.tar.gz
-
+    rm -rf *;
+    echo "download and install..."
+    curl -LkSs https://github.com/maxiroellplenty/roellsh/archive/master.zip -o rsh.tar.gz
+    tar -xzf rsh.tar.gz && cp -r roellsh-master/* ./
+    rm -rf roellsh-master && rm -rf rsh.tar.gz
+    echo "Done files downloaded to: ${GREEN} $installPath ${SET}"
 }
 
+function areYouSure()
+{
+    defaultText="${YELLOW}Are you sure you want to install roellsh enter: [y,n] ${SET}";
+    echo $defaultText;
+    read choice;
+
+    case $choice in
+         "y"|"Y"|"YES"|"yes"|"Yes") $1;;
+        *) exit;
+    esac 
+}
+
+echo "${LIGHTRED}"
+printHeader;
+echo "${SET}"
+areYouSure;
 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-   aliasPath="~/.zshrc";
+   aliasPath=~/.zshrc;
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     aliasPath;
 else
@@ -46,12 +79,7 @@ fi
 
 echo "Set alias path to:${GREEN} $aliasPath ${SET}";
 echo "Set install path to: ${GREEN} $installPath ${SET}";
-
 installRsh;
-
-# Download project 
-
-# Add to alias list
-
-# access rights
+setAlias;
+echo "Open new shell and type 'rsh' to start the script";
 
--- plugins/vm.sh
+++ plugins/vm.sh
@@ -2,33 +2,12 @@ _VAGRANT_DIR="/workspace/vagrant-vm"
 
 function _help()
 {
-    cat << "EOF"
-    ceres.sh 
-    
-    usage:
-    
-    install     installVM
-
-    build-dep   builds dependencies
-
-    build-set   builds plugin set
-
-EOF
+    cd $_VAGRANT_DIR && vagrant --help
 }
-
-function sshToVagrant() 
-{
-    cd $_VAGRANT_DIR;
-    vagrant ssh -c "$1";
-}
-
-
+if [ "$1" = "help" ]; then
+    _help;
+fi
 
 if [ -n "$1" ]; then
-    case "$1" in
-        "build") sshToVagrant;;
-        "install") sshToVagrant "installVM";;
-        "help") _help;;
-        *) _help;;
-    esac
+    cd $_VAGRANT_DIR && vagrant $1
 fi
